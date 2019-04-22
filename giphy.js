4f2354bbccf92ecc236b689ba5332187da6799ac
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-22 06:22:36 -0400

    added buttons, api, and loop to js

--- giphy.js
+++ giphy.js
@@ -8,4 +8,32 @@ for (var i=0; i< topics.length ;i++){
     $("<button>").appendTo(buttonarea); 
 }
 
+var topics = ["Flowers","Patriots","Slay","Lemonade","Money","Laugh"];
+var queryURL = "https://api.giphy.com//v1/gifs/search?=" + topics[i] + "&api_key=" + APIkey + "&limit=10" ;
+var APIkey = "tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy" ;
+// https://developers.giphy.com/docs/#path--gifs-search
+
+
+
+// click-json-solved.html
+for (var i=0; i< topics.length ;i++){
+   var newtopicbtn = $("<button>" + topics[i] +"</button>")
+   $("#buttonarea").append(newtopicbtn); 
+}
+
+
+function displayGIFS() {
+
+
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com//v1/gifs/search?=" + topics[i] + "&api_key=" + APIkey + "&limit=10" ;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $("#gifarea").text(JSON.stringify(response));
+    });
+}
+
 

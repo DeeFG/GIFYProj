350b4153f7eac5cada661765d95f4b41986ed027
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-22 03:29:48 -0400

    structure,links,

---
+++ giphy.js
@@ -0,0 +1,11 @@
+var topics = ["keto", "travel","patriots","slay","lemonade","money","laugh"];
+
+Api Key:
+tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy
+
+
+for (var i=0; i< topics.length ;i++){
+    $("<button>").appendTo(buttonarea); 
+}
+
+

a40c16f0096065e4b33105527f9d00f9da20f170
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-22 23:05:16 -0400

    added working api and onclick

--- giphy.js
+++ giphy.js
@@ -1,39 +1,78 @@
-var topics = ["keto", "travel","patriots","slay","lemonade","money","laugh"];
-
-Api Key:
-tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy
-
-
-for (var i=0; i< topics.length ;i++){
-    $("<button>").appendTo(buttonarea); 
-}
-
 var topics = ["Flowers","Patriots","Slay","Lemonade","Money","Laugh"];
-var queryURL = "https://api.giphy.com//v1/gifs/search?=" + topics[i] + "&api_key=" + APIkey + "&limit=10" ;
-var APIkey = "tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy" ;
-// https://developers.giphy.com/docs/#path--gifs-search
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy&limit=10";
+// var queryURL = "https://api.giphy.com//v1/gifs/search?q=" + topics[i] + "&api_key=tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy&limit=10" ;
+// var APIkey = "tWQYz0gCQxDLPiDqZLzGWA4Qpb5NkuSy" ;
 
 
+ $("#buttonarea").empty();
 
-// click-json-solved.html
-for (var i=0; i< topics.length ;i++){
-   var newtopicbtn = $("<button>" + topics[i] +"</button>")
-   $("#buttonarea").append(newtopicbtn); 
-}
-
+    for (var i = 0; i < topics.length; i++) {
+        var newtopicbtn = $("<button >" + topics[i] + "</button>")
+        newtopicbtn.addClass("gif");
+        newtopicbtn.attr("data-topic", topics[i]);
+        newtopicbtn.text(topics[i]);
+        $("#buttonarea").append(newtopicbtn);
+     displayGIFS();
+    }
 
 function displayGIFS() {
-
-
-    var topic = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com//v1/gifs/search?=" + topics[i] + "&api_key=" + APIkey + "&limit=10" ;
+    newtopicbtn = $(this).attr("data-topic");
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        $("#gifarea").text(JSON.stringify(response));
+        $("#gifarea").html(JSON.stringify(response.data));
+
+//         $('#img').html('<img src="http://www.site.com/' + $(this).val() + '.jpg"
+// width = "200px" > ');
+//         $('#img').hide().fadeIn('slow');
+
+        console.log(response.data[type])
+        console.log(response.url)
+        console.log()
+
+
+
     });
 }
 
+
+$("#button-addon2").on("click", function(event){
+    //event.preventDefualt();
+
+    var topic = $("#form-control").val().trim();
+    topics.push(topic);
+    console.log(movies);
+
+    getBtn();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// https://developers.giphy.com/docs/#path--gifs-search
+
+// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+// Under every gif, display its rating (PG, G, so on).
+
+// This data is provided by the GIPHY API.
+// Only once you get images displaying with button presses should you move on to the next step.
+// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+
+// Deploy your assignment to Github Pages.
+
+// Rejoice! You just made something really cool.
 
